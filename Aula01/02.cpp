4440842cdf0b661d3fab8e6b8786ddc6e5aabaa2
Arantees <lukasarantes59@gmail.com>
2023-09-04 20:00:09 -0300

    alteração

--- Aula01/02.cpp
+++ Aula01/02.cpp
@@ -9,15 +9,13 @@ Exemplo de funções e passagem de parâmetros
 void nao_troca_valores(int valor_1, int valor_2);
 void troca_valores(int *ponteiro_1, int *ponteiro_2);
 
-void funcao_1_1()
-{
+void funcao_1_1(){
     float b;
     b = 7;
     cout << "Dentro da funcao_1_1: " << b << endl;
 }
 
-void funcao_1()
-{
+void funcao_1(){
     float a;
     a = 4.5;
     funcao_1_1();
@@ -25,8 +23,8 @@ void funcao_1()
     funcao_1_1();
 }
 
-int main(int argc, char *argv[])
-{
+
+int main(int argc, char *argv[]){
     int numero_1;
     int numero_2;
 
@@ -37,34 +35,32 @@ int main(int argc, char *argv[])
     cout << " Digite primero numero " << endl;
     cin >> numero_2;
 
-    cout << "Numeros digitados: " << numero_1 << "  " << numero_2 << endl;
+    cout << "Numeros digitados: " <<  numero_1 << "  " << numero_2 << endl;
 
     nao_troca_valores(numero_1, numero_2);
-    cout << "Numeros digitados: " << numero_1 << "  " << numero_2 << endl;
+    cout << "Numeros digitados: "<<  numero_1 << "  " << numero_2 << endl;
 
     troca_valores(&numero_1, &numero_2);
-    cout << "Numeros digitados: " << numero_1 << "  " << numero_2 << endl;
+    cout << "Numeros digitados: "<<  numero_1 << "  " << numero_2 << endl;
 
     return 0;
 }
 
 /* Passagem por valor */
-void nao_troca_valores(int valor_1, int valor_2)
-{
+void nao_troca_valores(int valor_1, int valor_2) {
     int auxiliar;
     auxiliar = valor_1;
     valor_1 = valor_2;
     valor_2 = auxiliar;
-    cout << "Dentro da funcao nao_troca_valores: " << valor_1 << "  " << valor_2 << endl;
+    cout <<"Dentro da funcao nao_troca_valores: "<< valor_1 << "  " <<  valor_2 << endl;
     return;
 }
 /* Passagem por referência */
-void troca_valores(int *ponteiro_1, int *ponteiro_2)
-{
+void troca_valores(int *ponteiro_1, int *ponteiro_2) {
     int auxiliar;
     auxiliar = *ponteiro_1;
     *ponteiro_1 = *ponteiro_2;
     *ponteiro_2 = auxiliar;
-    cout << "Dentro da funcao nao_troca_valores: " << *ponteiro_1 << "  " << *ponteiro_2 << endl;
+    cout <<"Dentro da funcao nao_troca_valores: "<< *ponteiro_1 << "  " <<  *ponteiro_2 << endl;
     return;
 }
--- Aula02/03_A.c
+++ Aula02/03_A.c
@@ -15,10 +15,8 @@ string recebe_disciplina_aluno();
 float recebe_nota_1();
 float recebe_nota_2();
 
-int main(int argc, char *argv[])
-{
-    struct cadastro
-    {
+int main(int argc, char *argv[]){
+    struct cadastro {
         string nome;
         string disciplina;
         float nota_1;
@@ -33,44 +31,30 @@ int main(int argc, char *argv[])
     aluno1.nota_1 = recebe_nota_1();
     aluno1.nota_2 = recebe_nota_2();
 
-    struct cadastro aluno2;
-    aluno2.nome = "José";
-    aluno2.disciplina = "ED";
-    aluno2.nota_1 = 7;
-    aluno2.nota_2 = 9;
-    aluno2.media_simples = (aluno2.nota_1 + aluno2.nota_2) / 2;
-
     cout << "Nome = " << aluno1.nome << endl;
     cout << "Disciplina = " << aluno1.disciplina << endl;
     cout << "Nota 1 = " << aluno1.nota_1 << endl;
     cout << "Nota 2 = " << aluno1.nota_2 << endl;
     aluno1.media_simples = (aluno1.nota_1 + aluno1.nota_2) / 2;
-    cout << "Média Simples = " << aluno1.media_simples << endl;
-
-    cout << "===========" << endl;
-    cout << "Nome = " << aluno2.nome << endl;
-    cout << "Média simples: " << aluno2.media_simples << endl;
+    cout << "Média Simples = " << aluno1.media_simples  << endl;
     return 0;
 }
 
-string recebe_nome_aluno()
-{
+string recebe_nome_aluno() {
     string nome_recebido;
     cout << "Digite o nome do aluno: ";
     cin >> nome_recebido;
     return nome_recebido;
 }
 
-string recebe_disciplina_aluno()
-{
+string recebe_disciplina_aluno() {
     string disciplina;
     cout << "Digite a disciplina cursada: ";
     cin >> disciplina;
     return disciplina;
 }
 
-float recebe_nota_1()
-{
+float recebe_nota_1() {
     string nota;
     float nota_1;
     cout << "Digite a nota do aluno: ";
@@ -78,8 +62,7 @@ float recebe_nota_1()
     return nota_1;
 }
 
-float recebe_nota_2()
-{
+float recebe_nota_2() {
     string nota;
     float nota_2;
     cout << "Digite a nota do aluno: ";
