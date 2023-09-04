bafbbf551caedaa94c219fb2349a2b7587278b18
Arantees <lukasarantes59@gmail.com>
2023-09-04 19:55:50 -0300

    .

--- Aula01/03.cpp
+++ Aula01/03.cpp
@@ -2,17 +2,22 @@
 
 using namespace std;
 
-int fatorial(int n) {
-    if ((n==1) || (n==0)){
+int fatorial(int n)
+{
+    if ((n == 1) || (n == 0))
+    {
         return 1;
-    } else {
-        return fatorial(n-1)*n;
+    }
+    else
+    {
+        return fatorial(n - 1) * n;
     }
 }
 
-int main () {
+int main()
+{
     int n;
     cout << "Informe o número que deseja obter o fatorial:" << endl;
     cin >> n;
-    cout << "O fatorial de " << n << " eh " << fatorial(n) << endl;
+    cout << "O fatorial de " << n << " é " << fatorial(n) << endl;
 }
---
+++ Aula02/exercicio1.c
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    struct cadastro
+    {
+        string descricao
+    } cadastro carro
+        carro.descricao = "procura no google";
+    return 0;
+}
